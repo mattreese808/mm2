6eff4f61f7c52fc04d8a25c90c9dc92440ffa24e
mattreese808 <matt808@icloud.com>
2016-01-12 11:02:06 -1000

    change lang

--- js/news/wordaday.js
+++ js/news/wordaday.js
@@ -1,20 +1,20 @@
 // A lot of this code is from the original feedToJson function that was included with this project
 // The new code allows for multiple feeds to be used but a bunch of variables and such have literally been copied and pasted into this code and some help from here: http://jsfiddle.net/BDK46/
 // The original version can be found here: http://airshp.com/2011/jquery-plugin-feed-to-json/
-var news = {
-	feed: config.news.feed || null,
-	newsLocation: '.news',
-	newsItems: [],
-	seenNewsItem: [],
+var wordaday = {
+	feed: config.wordaday.feed || null,
+	wordadayLocation: '.wordaday',
+	wordadayItems: [],
+	seenwordadayItem: [],
 	_yqURL: 'http://query.yahooapis.com/v1/public/yql',
 	_yqlQS: '?format=json&q=select%20*%20from%20rss%20where%20url%3D',
 	_cacheBuster: Math.floor((new Date().getTime()) / 1200 / 1000),
 	_failedAttempts: 0,
-	fetchInterval: config.news.fetchInterval || 60000,
-	updateInterval: config.news.interval || 5500,
-	fadeInterval: 2000,
+	fetchInterval: config.wordaday.fetchInterval || 43200000,
+	updateInterval: config.wordaday.interval || 55,
+	fadeInterval: 200,
 	intervalId: null,
-	fetchNewsIntervalId: null
+	fetchwordadayIntervalId: null
 }
 
 /**
@@ -22,19 +22,19 @@ var news = {
  * @param  {string} feed The original location of the RSS feed
  * @return {string}      The new location of the RSS feed provided by Yahoo
  */
-news.buildQueryString = function (feed) {
+wordaday.buildQueryString = function (feed) {
 
 	return this._yqURL + this._yqlQS + '\'' + encodeURIComponent(feed) + '\'';
 
 }
 
 /**
- * Fetches the news for each feed provided in the config file
+ * Fetches the wordaday for each feed provided in the config file
  */
-news.fetchNews = function () {
+wordaday.fetchwordaday = function () {
 
-	// Reset the news feed
-	this.newsItems = [];
+	// Reset the wordaday feed
+	this.wordadayItems = [];
 
 	this.feed.forEach(function (_curr) {
 
@@ -49,7 +49,7 @@ news.fetchNews = function () {
  * Runs a GET request to Yahoo's service
  * @param  {string} yqUrl The URL being used to grab the RSS feed (in JSON format)
  */
-news.fetchFeed = function (yqUrl) {
+wordaday.fetchFeed = function (yqUrl) {
 
 	$.ajax({
 		type: 'GET',
@@ -73,64 +73,64 @@ news.fetchFeed = function (yqUrl) {
 }
 
 /**
- * Parses each item in a single news feed
- * @param  {Object} data The news feed that was returned by Yahoo
+ * Parses each item in a single wordaday feed
+ * @param  {Object} data The wordaday feed that was returned by Yahoo
  * @return {boolean}      Confirms that the feed was parsed correctly
  */
-news.parseFeed = function (data) {
+wordaday.parseFeed = function (data) {
 
 	var _rssItems = [];
 
 	for (var i = 0, count = data.length; i < count; i++) {
 
-		_rssItems.push(data[i].title);
+		_rssItems.push(data[i].description + '\t' + data[i].title);
 
 	}
 
-	this.newsItems = this.newsItems.concat(_rssItems);
+	this.wordadayItems = this.wordadayItems.concat(_rssItems);
 
 	return true;
 
 }
 
 /**
- * Loops through each available and unseen news feed after it has been retrieved from Yahoo and shows it on the screen
- * When all news titles have been exhausted, the list resets and randomly chooses from the original set of items
- * @return {boolean} Confirms that there is a list of news items to loop through and that one has been shown on the screen
+ * Loops through each available and unseen wordaday feed after it has been retrieved from Yahoo and shows it on the screen
+ * When all wordaday titles have been exhausted, the list resets and randomly chooses from the original set of items
+ * @return {boolean} Confirms that there is a list of wordaday items to loop through and that one has been shown on the screen
  */
-news.showNews = function () {
+wordaday.showwordaday = function () {
 
 	// If all items have been seen, swap seen to unseen
-	if (this.newsItems.length === 0 && this.seenNewsItem.length !== 0) {
+	if (this.wordadayItems.length === 0 && this.seenwordadayItem.length !== 0) {
 
 		if (this._failedAttempts === 20) {
-			console.error('Failed to show a news story 20 times, stopping any attempts');
+			console.error('Failed to show a wordaday story 20 times, stopping any attempts');
 			return false;
 		}
 
 		this._failedAttempts++;
 
 		setTimeout(function () {
-			this.showNews();
+			this.showwordaday();
 		}.bind(this), 3000);
 
-	} else if (this.newsItems.length === 0 && this.seenNewsItem.length !== 0) {
-		this.newsItems = this.seenNewsItem.splice(0);
+	} else if (this.wordadayItems.length === 0 && this.seenwordadayItem.length !== 0) {
+		this.wordadayItems = this.seenwordadayItem.splice(0);
 	}
 
-	var _location = Math.floor(Math.random() * this.newsItems.length);
+	var _location = Math.floor(Math.random() * this.wordadayItems.length);
 
-	var _item = news.newsItems.splice(_location, 1)[0];
+	var _item = wordaday.wordadayItems.splice(_location, 1)[0];
 
-	this.seenNewsItem.push(_item);
+	this.seenwordadayItem.push(_item);
 
-	$(this.newsLocation).updateWithText(_item, this.fadeInterval);
+	$(this.wordadayLocation).updateWithText(_item, this.fadeInterval);
 
 	return true;
 
 }
 
-news.init = function () {
+wordaday.init = function () {
 
 	if (this.feed === null || (this.feed instanceof Array === false && typeof this.feed !== 'string')) {
 		return false;
@@ -138,15 +138,15 @@ news.init = function () {
 		this.feed = [this.feed];
 	}
 
-	this.fetchNews();
-	this.showNews();
+	this.fetchwordaday();
+	this.showwordaday();
 
-	this.fetchNewsIntervalId = setInterval(function () {
-		this.fetchNews()
+	this.fetchwordadayIntervalId = setInterval(function () {
+		this.fetchwordaday()
 	}.bind(this), this.fetchInterval)
 
 	this.intervalId = setInterval(function () {
-		this.showNews();
+		this.showwordaday();
 	}.bind(this), this.updateInterval);
 
 }
